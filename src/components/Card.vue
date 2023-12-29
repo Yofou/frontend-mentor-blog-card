c677d8dad441a1192c1959bb16c4d9e8ae26fb20
Nathan <reach@yofou.dev>
2023-12-29 05:44:09 -0800

    fix: use flex gap instead of margin top

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,26 +1,34 @@
 <template>
 	<section
-		class="max-w-[384px] w-full p-6 rounded-[1.25rem] bg-white outline-1 outline outline-black [box-shadow:8px_8px_0px_0px_#000] flex flex-col"
+		class="max-w-[384px] w-full p-6 rounded-[1.25rem] bg-white border border-black [box-shadow:8px_8px_0px_0px_#000] flex flex-col gap-6"
 	>
-		<div class="w-full h-[12.5rem] bg-yellow rounded-[.625rem] overflow-hidden">
-			<CardHeader class="w-full" preserveAspectRatio="xMinYMin slice"  />
+		<div
+			class="w-full h-[12.5rem] bg-yellow rounded-[.625rem] overflow-hidden"
+		>
+			<CardHeader class="w-full" preserveAspectRatio="xMinYMin slice" />
 		</div>
 
-		<p
-			class="py-[.25rem] text-black font-extrabold px-[.75rem] bg-yellow rounded-[.25rem] self-start mt-[1.5rem] body-s"
-		>
-			Learning
-		</p>
+		<div class="flex flex-col gap-[.75rem]">
+			<p
+				class="py-[.25rem] text-black font-extrabold px-[.75rem] bg-yellow rounded-[.25rem] self-start body-s"
+			>
+				Learning
+			</p>
 
-		<p class="mt-[.75rem] font-medium body-s">Published 21 Dec 2023</p>
-		<a class="mt-[.75rem] heading font-extrabold text-black hover:text-yellow transition-colors" href="/">HTML & CSS foundations</a>
+			<p class="font-medium body-s">Published 21 Dec 2023</p>
+			<a
+				class="heading font-extrabold text-black hover:text-yellow transition-colors"
+				href="/"
+				>HTML & CSS foundations</a
+			>
 
-		<p class="text-grey mt-[.75rem] font-medium body-m">
-			These languages are the backbone of every website, defining
-			structure, content, and presentation.
-		</p>
+			<p class="text-grey font-medium body-m">
+				These languages are the backbone of every website, defining
+				structure, content, and presentation.
+			</p>
+		</div>
 
-		<div class="flex gap-[.75rem] mt-6 items-center">
+		<div class="flex gap-[.75rem] items-center">
 			<img class="w-8 h-8" src="/userpic.png" alt="" />
 			<p class="body-s font-extrabold">Greg Hooper</p>
 		</div>
@@ -28,5 +36,5 @@
 </template>
 
 <script setup lang="ts">
-	import CardHeader from "./CardHeader.vue";
+import CardHeader from "./CardHeader.vue";
 </script>
