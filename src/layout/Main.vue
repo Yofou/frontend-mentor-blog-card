c2c2fcf78b0c38a3b26e97b8c62d48e1fd431c2c
Nathan <reach@yofou.dev>
2023-12-28 18:10:56 -0800

    chore: added top level heading

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -1,4 +1,5 @@
 <template>
+	<h1 class="sr-only">Frontend mentor blog card</h1>
 	<main class="w-full min-h-screen grid justify-center items-start px-6 md:px-0 py-[9.75rem] md:place-items-center bg-yellow">
 		<slot />
 	</main>
